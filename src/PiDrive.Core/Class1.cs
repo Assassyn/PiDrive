c6e8d079592bf793bee13663774a169d1c93c392
Szymon Sasin <szymon@sasin.eu>
2017-05-20 22:53:58 +0100

    moved binary files

--- src/PiDrive.Core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace PiDrive.Core
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/PiDrive.Core/Library.fs
@@ -0,0 +1,5 @@
+namespace PiDrive.Core
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
